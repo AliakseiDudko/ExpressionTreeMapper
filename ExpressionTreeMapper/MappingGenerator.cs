467add1d0d4964fd2d59336756e7d0695591be7f
aleksei.dudko@gmail.com <aleksei.dudko@gmail.com>
2018-10-13 23:24:30 +0300

    Implement mapping logic.

--- ExpressionTreeMapper/MappingGenerator.cs
+++ ExpressionTreeMapper/MappingGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Reflection;
 
 namespace ExpressionTreeMapper
 {
@@ -7,12 +9,47 @@ namespace ExpressionTreeMapper
     {
         public Mapper<TSource, TDestination> Generate<TSource, TDestination>()
         {
-            var sourceParam = Expression.Parameter(typeof(TSource));
-            var mapFunction = Expression.Lambda<Func<TSource, TDestination>>(
-                Expression.New(typeof(TDestination)),
-                sourceParam);
+            var source = Expression.Parameter(typeof(TSource), "source");
+            var destination = Expression.Parameter(typeof(TDestination), "destination");
+
+            var mapExpressionBody = BuildMapExpressionBody<TSource, TDestination>(source, destination);
+            var mapFunction = Expression.Lambda<Func<TSource, TDestination>>(mapExpressionBody, source);
 
             return new Mapper<TSource, TDestination>(mapFunction.Compile());
+        }
+
+        private Expression BuildMapExpressionBody<TSource, TDestination>(ParameterExpression source, ParameterExpression destination)
+        {
+            var statements = new List<Expression>();
+            statements.Add(Expression.Assign(destination, Expression.New(typeof(TDestination))));
+            statements.AddRange(GetPropertyMappingExpresions<TSource>(source, destination));
+            statements.Add(destination);
+
+            return Expression.Block(new[] { destination }, statements);
+        }
+
+        private IEnumerable<Expression> GetPropertyMappingExpresions<TSource>(ParameterExpression source, ParameterExpression destination)
+        {
+            var mapExpressions = new List<Expression>();
+
+            var propertyInfos = typeof(TSource).GetProperties(BindingFlags.Instance | BindingFlags.Public | BindingFlags.SetProperty);
+            foreach (var propertyInfo in propertyInfos)
+            {
+                var sourceProperty = Expression.Property(source, propertyInfo.Name);
+                var targetProperty = Expression.Property(destination, propertyInfo.Name);
+
+                Expression value = sourceProperty;
+                if (value.Type != targetProperty.Type)
+                {
+                    value = Expression.Convert(value, targetProperty.Type);
+                }
+
+                Expression statement = Expression.Assign(targetProperty, value);
+
+                mapExpressions.Add(statement);
+            }
+
+            return mapExpressions;
         }
     }
 }
