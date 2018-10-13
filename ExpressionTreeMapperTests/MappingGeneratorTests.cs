86896aa799814877fecb026c722d797adaab768e
aleksei.dudko@gmail.com <aleksei.dudko@gmail.com>
2018-10-13 19:28:51 +0300

    Add empty classes.

---
+++ ExpressionTreeMapperTests/MappingGeneratorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExpressionTreeMapper;
+
+namespace ExpressionTreeMapperTest
+{
+    public class Foo { }
+    public class Bar { }
+
+    [TestClass]
+    public class MappingGeneratorTests
+    {
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var mapGenerator = new MappingGenerator();
+            var mapper = mapGenerator.Generate<Foo, Bar>();
+
+            var res = mapper.Map(new Foo());
+        }
+    }
+}
